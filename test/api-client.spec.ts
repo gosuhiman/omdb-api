419d85f177eae432e83b8474a7a49b98c972e560
gosuhiman <gosuhiman@gmail.com>
2019-02-17 20:50:58 +0100

    fix tests

--- test/api-client.spec.ts
+++ test/api-client.spec.ts
@@ -1,36 +1,27 @@
-require('dotenv').config();
-const expect = require('chai').expect;
+import {expect} from 'chai';
+import * as dotenv from 'dotenv';
+import {OmdbGetResult, OmdbSearchResult} from "../lib";
+
+dotenv.config();
 
 describe('API Client', () => {
   const OmdbApiClient = require('../lib/index').OmdbApiClient;
   const API_KEY: string = process.env.OMDB_API_KEY;
   const client = new OmdbApiClient(API_KEY);
 
-  it('getByTitle response has Title, Year and Genre fields', () => {
-    client.getByTitle('Star Wars')
-      .then(result => {
-        expect(result).to.have.all.keys('Title', 'Year', 'Genre');
-      })
-      .catch(() => {
-      });
+  it('getByTitle(Star Wars) returns movie with title Star Wars: Episode IV - A New Hope', async () => {
+    const result: OmdbGetResult = await client.getByTitle('Star Wars');
+    expect(result.Title).to.equal('Star Wars: Episode IV - A New Hope');
   });
 
-  it('getByImdbId response has Title, Year and Genre fields', () => {
-    client.getByImdbId('tt0076759')
-      .then(result => {
-        expect(result).to.have.all.keys('Title', 'Year', 'Genre');
-      })
-      .catch(() => {
-      });
+  it('getByImdbId(tt0076759) returns movie with title Star Wars: Episode IV - A New Hope', async () => {
+    const result: OmdbGetResult = await client.getByImdbId('tt0076759');
+    expect(result.Title).to.equal('Star Wars: Episode IV - A New Hope');
   });
 
-  it('search response has more then 3 movies with correct fields', () => {
-    client.search('Star Wars')
-      .then(results => {
-        expect(results).to.have.lengthOf.at.least(3);
-        expect(results[0]).to.have.all.keys('Title', 'Year', 'Genre');
-      })
-      .catch(() => {
-      });
+  it('search response has more then 3 movies with correct fields', async () => {
+    const result: OmdbSearchResult = await client.search('Star Wars');
+    expect(result.Search).to.have.lengthOf.at.least(3);
+    expect(result.Search[0]).to.have.keys('Title', 'Year', 'Type', 'imdbID', 'Poster');
   });
 });
